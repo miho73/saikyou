012f48277494aaa2b7b0fbd43345156035337642
miho73 <hyrexfiels@gmail.com>
2026-05-03 05:08:25 +0900

    타임서버 필요한 것만 보내는걸로 바꿈

--- idol/app/routers/TimeRouter.py
+++ idol/app/routers/TimeRouter.py
@@ -20,25 +20,13 @@ async def clock():
 
   client = ntplib.NTPClient()
 
-  google_response = client.request("time.google.com", version=3)
-  ntp_time_google = google_response.tx_time
-  log.info("Google time fetched", extra={"ntp_time_google": ntp_time_google})
-
   kriss_response = client.request("ntp.kriss.re.kr", version=3)
   ntp_time_kriss = kriss_response.tx_time
   log.info("KRISS time fetched", extra={"ntp_time_kriss": ntp_time_kriss})
 
-  nist_response = client.request("time.nist.gov", version=3)
-  ntp_time_nist = nist_response.tx_time
-  log.info("NIST time fetched", extra={"ntp_time_nist": ntp_time_nist})
-
-  dt_google = datetime.fromtimestamp(ntp_time_google).astimezone()
   dt_kriss = datetime.fromtimestamp(ntp_time_kriss).astimezone()
-  dt_nist = datetime.fromtimestamp(ntp_time_nist).astimezone()
 
   return {
     "time": date.isoformat(),
-    "google": dt_google.isoformat(),
     "kriss": dt_kriss.isoformat(),
-    "nist": dt_nist.isoformat(),
   }
